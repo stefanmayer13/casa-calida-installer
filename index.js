ab298456dd13445136e5ff0c8dbc7f9ac9436d5d
Stefan Mayer <stefanmayer13@gmail.com>
2016-01-06 22:01:37 +0100

    Installing casa-linga and adding startup script to reboot

---
+++ index.js
@@ -0,0 +1,79 @@
+/**
+ * @author <a href="mailto:stefan@stefanmayer.me">Stefan Mayer</a>
+ */
+
+var exec = require('child_process').exec;
+var fs = require('fs');
+var path = require('path');
+var readLine = require('readline');
+var startScript = require('./startScript');
+
+var uuid = 'd7a0d395-ae02-482d-92fc-73357d515275';
+
+const cliArguments = process.argv.reduce(function (argObject, arg) {
+    var argument = arg.split('=');
+    if (argument.length === 2) {
+        argObject[argument[0]] = argument[1];
+    }
+    return argObject;
+}, {});
+
+if (!cliArguments['config']) {
+    console.error('Please provide "config" as arguments to point to your config file. See README for more infos.');
+    process.exit(1);
+}
+
+const absoluteConfigPath = path.resolve(cliArguments['config']);
+try {
+    const stats = fs.lstatSync(absoluteConfigPath);
+
+    if (stats.isDirectory()) {
+        throw new Error('config is a directory');
+    }
+} catch (e) {
+    console.error('Couldn\'t find provided config file ' + absoluteConfigPath);
+    process.exit(2);
+}
+
+console.log('Installing casa-calida.');
+exec('npm uninstall casa-calida', function () {
+    exec('npm install --production casa-calida',
+        function (error, stdout, stderr) {
+            console.log(stdout);
+            if (stderr) {
+                console.log('errors: ' + stderr);
+            }
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            } else {
+                start(/^win/.test(process.platform));
+            }
+        });
+});
+
+function start(windows) {
+    if (windows) {
+        console.log('Windows system detected. Not using crontab.');
+    } else {
+        console.log('Non-Windows system detected. Using crontab.');
+
+        require('crontab').load(function(err, crontab) {
+            if (err) {
+                return console.error(err);
+            }
+
+            var nodePath = process.execPath.split('/').slice(0, -1).join('/');
+            var exportCommand = 'export PATH=' + nodePath + ':$PATH';
+            var foreverCommand = require('path').join(__dirname, 'node_modules', 'forever', 'bin', 'forever');
+            var sysCommand = exportCommand + ' && ' + foreverCommand + ' start node_modules/.bin/casa-calida -- config=' + cliArguments['config'];
+
+            crontab.remove({comment:uuid});
+            crontab.create(sysCommand, '@reboot', uuid);
+
+            crontab.save(function(err, crontab) {
+                console.log(err);
+            });
+        });
+    }
+    startScript(uuid, cliArguments['config']);
+}
---
+++ startScript.js
@@ -0,0 +1,64 @@
+/**
+ * @author <a href="mailto:stefan@stefanmayer.me">Stefan Mayer</a>
+ */
+
+var forever = require('forever');
+
+module.exports = function startScript(uuid, config) {
+    var casaCalidaCommand = require('path').join('node_modules', 'casa-calida', 'lib', 'index.js');
+
+    forever.list(false, function (err, data) {
+        if (data.filter(function (script) {
+                return script.uid === uuid && !!script.running;
+            }).length > 0) {
+            console.log('Stopping old process');
+            data.forEach(function (script, index) {
+                if(script.uid === uuid &&  !!script.running) {
+                    forever.stop(index);
+                }
+            });
+        }
+        forever.startDaemon(casaCalidaCommand, {
+            args: ['config=' + config],
+            uid: uuid,
+            max: 3,
+            silent: true,
+            'outFile': 'out.log',
+            'errFile': 'err.log'
+        });
+
+        setTimeout(function () {
+            console.log('Started forever. The process log till now:');
+            var errorLines = 0;
+
+            var logReader = readLine.createInterface({
+                input: require('fs').createReadStream('out.log')
+            });
+
+            logReader.on('line', function (line) {
+                console.log(line);
+            });
+
+            logReader.on('close', function () {
+                var logReader = readLine.createInterface({
+                    input: require('fs').createReadStream('err.log')
+                });
+
+                logReader.on('line', function (line) {
+                    errorLines++;
+                    console.error(line);
+                });
+
+                logReader.on('close', function () {
+                    if (errorLines === 0) {
+                        console.log('Everything worked fine.');
+                    } else {
+                        console.log('There seems to be a problem.');
+                    }
+                    console.log('Exiting installer');
+                    process.exit(0);
+                });
+            });
+        }, 5000);
+    });
+}
